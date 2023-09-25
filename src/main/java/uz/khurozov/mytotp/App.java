84339441c419f3f47c5ee4e2c9cb3a0ea7a5e69e
Ilyos Khurozov <ilyos@khurozov.uz>
2023-09-25 14:43:18 +0500

    changed resources path

--- src/main/java/uz/khurozov/mytotp/App.java
+++ src/main/java/uz/khurozov/mytotp/App.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("main.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/main.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("myTOTP");
         stage.setScene(scene);
